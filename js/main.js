969c9c9892dd451bc109c8f4ebda0ad64a083a73
Yegor Sawinkov <56470880+soulcather@users.noreply.github.com>
2025-02-07 21:56:24 +0200

    Add new sections + add in english version

--- js/main.js
+++ js/main.js
@@ -8,14 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const select = document.getElementById("language");
   const currentPage = window.location.pathname.split("/").pop(); // Получаем имя текущего файла
 
-  select.value = currentPage; // Устанавливаем правильный вариант в выпадающем списке
+  if (currentPage === "index.html") {
+    select.value = "index.html"; // Значение для Español
+  } else if (currentPage === "index_eng.html") {
+    select.value = "index_eng.html"; // Значение для English
+  }
 });
 
-// Initialize Flatpickr for both date inputs
+const userLang = document.documentElement.lang || "es"; // по умолчанию испанский
+
 flatpickr("#checkin", {
   dateFormat: "d F Y",
+  locale: userLang,
 });
 
 flatpickr("#checkout", {
   dateFormat: "d F Y",
+  locale: userLang,
 });
